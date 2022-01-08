a21816e840d41c7a5780bea861ab831d15e02d17
jc23729 <42083637+jc23729@users.noreply.github.com>
2022-01-08 12:51:09 -0500

    update

---
+++ back_to_react/src/Playground.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import randomColor from "randomcolor";
+
+export default function Playground() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      {count}
+      <button onClick={() => setCount(count - 1)}>-</button>
+    </div>
+  );
+}
--- back_to_react/src/index.js
+++ back_to_react/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Playground from './Playground'
 
 ReactDOM.render(
   <React.StrictMode>
